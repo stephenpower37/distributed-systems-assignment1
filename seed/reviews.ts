4b60593c6f38169bafd607d441bac3de2fe70a0e
Stephen Power <stephenxpower2003@gmail.com>
2023-11-25 11:30:22 +0000

    Seeded the movie reviews table with movie reviews where reviewerName is set to usernames rather than full names with whitespace

--- seed/reviews.ts
+++ seed/reviews.ts
@@ -2,20 +2,62 @@ import { Review } from "../shared/types";
 
 export const reviews: Review[] = [
     {
-        movieId: 5691,
-        reviewerName: "Stephen Power",
+        movieId: 2910,
+        reviewerName: "moviereviewer101",
+        reviewDate: "03/05/2023",
+        content: "I thought this movie could have been a bit longer with a better ending"
+    },
+    {
+        movieId: 9091,
+        reviewerName: "stephenpower2003",
         reviewDate: "06/02/2023",
+        content: "I really enjoyed this movie and thought the acting was excellent"
+    },
+    {
+        movieId: 3877,
+        reviewerName: "markoshea75",
+        reviewDate: "17/02/2023",
+        content: "I thought the movie dragged on and was very slow paced"
+    },
+    {
+        movieId: 2910,
+        reviewerName: "anonymous404",
+        reviewDate: "24/10/2023",
+        content: "I enjoyed this movie, but it felt like it was over before I knew it"
+    },
+    {
+        movieId: 6798,
+        reviewerName: "aoifeking99",
+        reviewDate: "08/08/2023",
+        content: "I enjoyed most of the movie, but the ending was a let down"
+    },
+    {
+        movieId: 3877,
+        reviewerName: "cormacosullivan2023",
+        reviewDate: "30/09/2023",
+        content: "I thought the movie was very long and could have easily been concised"
+    },
+    {
+        movieId: 2910,
+        reviewerName: "joemaxwell510",
+        reviewDate: "19/02/2023",
+        content: "I thought this movie was excellent and it was nice and concise"
+    },
+    {
+        movieId: 5691,
+        reviewerName: "lewishamilton86",
+        reviewDate: "06/11/2023",
         content: "Really enjoyed this movie"
     },
     {
-        movieId: 8736,
-        reviewerName: "John O'Shea",
+        movieId: 9091,
+        reviewerName: "maxverstappen33",
         reviewDate: "09/07/2023",
-        content: "Didn't find this movie entertaining and thought it was too long"
+        content: "Brilliant movie with a great cast"
     },
     {
         movieId: 1091,
-        reviewerName: "Simon Hill",
+        reviewerName: "simonhill95",
         reviewDate: "23/11/23",
         content: "Very good movie with great acting throughout"
     }
