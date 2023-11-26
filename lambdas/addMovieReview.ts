9477efe05fc48077da26a907f9e7552ad8ad604a
Stephen Power <stephenxpower2003@gmail.com>
2023-11-26 16:03:12 +0000

    Added JSON schema validation in the POST request body for adding a movie review

--- lambdas/addMovieReview.ts
+++ lambdas/addMovieReview.ts
@@ -1,6 +1,11 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import Ajv from "ajv";
+import schema from "../shared/types.schema.json";
+
+const ajv = new Ajv();
+const isValidBodyParams = ajv.compile(schema.definitions["Review"] || {});
 
 const ddbDocClient = createDDbDocClient();
 
@@ -16,6 +21,19 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
           "content-type": "application/json",
         },
         body: JSON.stringify({ message: "Missing request body" }),
+      };
+    }
+
+    if (!isValidBodyParams(body)) {
+      return {
+        statusCode: 500,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          message: `Incorrect type. Must match Review schema`,
+          schema: schema.definitions["Review"],
+        }),
       };
     }
 
